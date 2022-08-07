29993891a16ae8095b2979eda1b278cb9ac0cfe7
Moritz <moritz.lill@posteo.de>
2022-08-07 11:39:48 +0200

    add

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 
 import { IfcViewerAPI } from 'web-ifc-viewer';
-import {Color} from 'three';
+import { Color } from 'three';
 
 const container = document.getElementById('viewer-container');
-const viewer = new IfcViewerAPI({container, backgroundColor: new Color(0xffffff)});
+const viewer = new IfcViewerAPI({ container, backgroundColor: new Color(0xffffff) });
 viewer.axes.setAxes();
 viewer.grid.setGrid();
 
@@ -25,21 +25,21 @@ const clipperButton = document.getElementById('clipperButton');
 let clippingPlanesActive = false;
 clipperButton.onclick = () => {
   clippingPlanesActive = !clippingPlanesActive;
-   viewer.clipper.active = clippingPlanesActive;
+  viewer.clipper.active = clippingPlanesActive;
 
-   if(clippingPlanesActive) {
-     clipperButton.classList.add('active');
-   } else {
+  if (clippingPlanesActive) {
+    clipperButton.classList.add('active');
+  } else {
     clipperButton.classList.remove('active');
-   }
+  }
 }
 
 window.onmousemove = () => viewer.IFC.selector.prePickIfcItem();
 
 window.ondblclick = async () => {
-  if(clippingPlanesActive) {
+  if (clippingPlanesActive) {
     viewer.clipper.createPlane();
-  } else if(propertiesActive) {
+  } else if (propertiesActive) {
     const result = await viewer.IFC.selector.highlightIfcItem();
     if (!result) return;
     const { modelID, id } = result;
@@ -51,7 +51,7 @@ window.ondblclick = async () => {
 }
 
 window.onkeydown = (event) => {
-  if(event.code = 'delete' && clippingPlanesActive){
+  if (event.code = 'delete' && clippingPlanesActive) {
     viewer.clipper.deletePlane();
   }
 }
@@ -66,61 +66,63 @@ let propertiesActive = false;
 propertiesButton.onclick = () => {
   propertiesActive = !propertiesActive;
 
-   if(propertiesActive) {
+  if (propertiesActive) {
     propertiesButton.classList.add('active');
-      propsGUI.style.display = '';
-   } else {
+    propsGUI.style.display = '';
+  } else {
     propertiesButton.classList.remove('active');
     propsGUI.style.display = "none";
-   }
+  }
 }
 
 function createPropertiesMenu(properties) {
-    console.log(properties);
+  console.log(properties);
 
-    removeAllChildren(propsGUI);
+  removeAllChildren(propsGUI);
 
-    delete properties.psets;
-    delete properties.mats;
-    delete properties.type;
+  delete properties.psets;
+  delete properties.mats;
+  delete properties.type;
 
 
-    for (let key in properties) {
-        createPropertyEntry(key, properties[key]);
-    }
+  for (let key in properties) {
+    createPropertyEntry(key, properties[key]);
+  }
 
 }
 
 function createPropertyEntry(key, value) {
-    const propContainer = document.createElement("div");
-    propContainer.classList.add("ifc-property-item");
+  const propContainer = document.createElement("div");
+  propContainer.classList.add("ifc-property-item");
 
-    if(value === null || value === undefined) value = "undefined";
-    else if(value.value) value = value.value;
+  if (value === null || value === undefined) value = "undefined";
+  else if (value.value) value = value.value;
 
-    const keyElement = document.createElement("div");
-    keyElement.textContent = key;
-    propContainer.appendChild(keyElement);
+  const keyElement = document.createElement("div");
+  keyElement.textContent = key;
+  propContainer.appendChild(keyElement);
 
-    const valueElement = document.createElement("div");
-    valueElement.classList.add("ifc-property-value");
-    valueElement.textContent = value;
-    propContainer.appendChild(valueElement);
+  const valueElement = document.createElement("div");
+  valueElement.classList.add("ifc-property-value");
+  valueElement.textContent = value;
+  propContainer.appendChild(valueElement);
 
-    propsGUI.appendChild(propContainer);
+  propsGUI.appendChild(propContainer);
 }
 
 //functions
 
 function createTreeTable(ifcProject) {
-	
-	const tableRoot = document.getElementById('boq');
+
+  const tableRoot = document.getElementById('boq');
   removeAllChildren(tableRoot);
   populateIfcTable(tableRoot, ifcProject)
   implementTreeLogic();
 
-  // const buttonCollapse = document.getElementById('collapseButton')
-  // buttonCollapse.addEventListener('click', collapseTable)
+  const buttonCollapse = document.getElementById('collapseButton');
+  buttonCollapse.addEventListener('click', collapseTable);
+
+
   // const buttonExpand = document.getElementById('expandButton')
   // buttonExpand.addEventListener('click', expandTable);
 
@@ -128,16 +130,19 @@ function createTreeTable(ifcProject) {
 
 //Not working functions: 
 
-// function collapseTable() {
-//   const elements = document.getElementsByClassName('collapse');
-//   console.log(elements);  
-//   console.log(elements);  
-//   for (let element in elements) {
-//       console.log(element);
-//       // element.classList.remove('collapse');
-//       // element.classList.add('expand');
-//   }
-// }
+function collapseTable() {
+  const elements = document.getElementsByClassName('collapse');
+  elements[0].classList.remove('collapse');
+  elements[0].classList.add('expand');
+
+  for (let element in elements) {
+    if (element.length > 0) {
+
+      // element.classList.remove('collapse');
+      // element.classList.add('expand');
+    }
+  }
+}
 
 // function expandTable() {
 //   const elements = document.getElementsByClassName('expand');
@@ -150,69 +155,69 @@ function createTreeTable(ifcProject) {
 // }
 
 function populateIfcTable(table, ifcProject) {
-    const initialDepth = 0;
-    createNode(table, ifcProject, initialDepth, ifcProject.children);
+  const initialDepth = 0;
+  createNode(table, ifcProject, initialDepth, ifcProject.children);
 }
 
 
 function createNode(table, node, depth, children) {
 
-	if(children.length === 0) {
-		createLeafRow(table, node, depth);
-	} else {
-		// If there are multiple categories, group them together
-		const grouped = groupCategories(children);
-		createBranchRow(table, node, depth, grouped);
-	}
+  if (children.length === 0) {
+    createLeafRow(table, node, depth);
+  } else {
+    // If there are multiple categories, group them together
+    const grouped = groupCategories(children);
+    createBranchRow(table, node, depth, grouped);
+  }
 }
 
 function createBranchRow(table, node, depth, children) {
 
-    const row = document.createElement('tr');
-    const className = 'level' + depth;
-    row.classList.add(className);
-    row.classList.add('collapse');
-    row.setAttribute('data-depth', depth);
+  const row = document.createElement('tr');
+  const className = 'level' + depth;
+  row.classList.add(className);
+  row.classList.add('collapse');
+  row.setAttribute('data-depth', depth);
 
-    const dataName = document.createElement('td');
-
-
-    const toggle = document.createElement('span');
-    toggle.classList.add('toggle');
-    toggle.classList.add('collapse');
+  const dataName = document.createElement('td');
 
 
-    dataName.textContent = node.type;
-    dataName.insertBefore(toggle, dataName.firstChild);
+  const toggle = document.createElement('span');
+  toggle.classList.add('toggle');
+  toggle.classList.add('collapse');
 
-    row.appendChild(dataName);
-	  table.appendChild(row); 
 
-    depth = depth+1;
+  dataName.textContent = node.type;
+  dataName.insertBefore(toggle, dataName.firstChild);
 
-	children.forEach(child => createNode(table, child, depth, child.children ));
+  row.appendChild(dataName);
+  table.appendChild(row);
+
+  depth = depth + 1;
+
+  children.forEach(child => createNode(table, child, depth, child.children));
 
 }
 
 
 
 function createLeafRow(table, node, depth) {
-	const row = document.createElement('tr');
-    const className = 'level'+ depth;
-    row.classList.add(className);
-    row.classList.add('collapse');
-    row.setAttribute('data-depth', depth);
+  const row = document.createElement('tr');
+  const className = 'level' + depth;
+  row.classList.add(className);
+  row.classList.add('collapse');
+  row.setAttribute('data-depth', depth);
 
-    const dataName = document.createElement('td');
-    dataName.textContent = node.type;
-    const dataId = document.createElement('td');
-    dataId.textContent = node.expressID;
-    row.appendChild(dataName);
-    const price = document.createElement('td');
-    price.textContent = 'Area';
-    row.appendChild(dataId);
-    row.appendChild(price);
-	table.appendChild(row);
+  const dataName = document.createElement('td');
+  dataName.textContent = node.type;
+  const dataId = document.createElement('td');
+  dataId.textContent = node.expressID;
+  row.appendChild(dataName);
+  const price = document.createElement('td');
+  price.textContent = 'Area';
+  row.appendChild(dataId);
+  row.appendChild(price);
+  table.appendChild(row);
 
   row.onmouseenter = () => {
     viewer.IFC.selector.prepickIfcItemsByID(0, [node.expressID]);
@@ -225,82 +230,83 @@ function createLeafRow(table, node, depth) {
 }
 
 function groupCategories(children) {
-	const types = children.map(child => child.type);
-	const uniqueTypes = new Set(types);
-	if (uniqueTypes.size > 1) {
-		const uniquesArray = Array.from(uniqueTypes);
-		children = uniquesArray.map(type => {
-			return {
-				expressID: -1,
-				type: type + 'S',
-				children: children.filter(child => child.type.includes(type)),
-			};
-		});
-	}
-	return children;
+  const types = children.map(child => child.type);
+  const uniqueTypes = new Set(types);
+  if (uniqueTypes.size > 1) {
+    const uniquesArray = Array.from(uniqueTypes);
+    children = uniquesArray.map(type => {
+      return {
+        expressID: -1,
+        type: type + 'S',
+        children: children.filter(child => child.type.includes(type)),
+      };
+    });
+  }
+  return children;
 }
 
 //Collapsable table logic
 function implementTreeLogic() {
-[].forEach.call(document.querySelectorAll('#boq .toggle'), function(el) {
-    el.addEventListener('click', function() {
+  [].forEach.call(document.querySelectorAll('#boq .toggle'), function (el) {
+    el.addEventListener('click', function () {
       var el = this;
       var tr = el.closest('tr');
       var children = findChildren(tr);
-      var subnodes = children.filter(function(element) {
+      var subnodes = children.filter(function (element) {
         return element.matches('.expand');
       });
-      subnodes.forEach(function(subnode) {
+      subnodes.forEach(function (subnode) {
         var subnodeChildren = findChildren(subnode);
-        children = children.filter(function(element) {
-            return !subnodeChildren.includes(element);
+        children = children.filter(function (element) {
+          return !subnodeChildren.includes(element);
         });
-              console.log(children);
+        console.log(children);
         //children = children.not(subnodeChildren);
       });
       if (tr.classList.contains('collapse')) {
         tr.classList.remove('collapse');
         tr.classList.add('expand');
-        children.forEach(function(child) {
+        children.forEach(function (child) {
           child.style.display = 'none';
         });
       } else {
         tr.classList.remove('expand');
         tr.classList.add('collapse');
-        children.forEach(function(child) {
+        children.forEach(function (child) {
           child.style.display = '';
         });
       }
     })
-  })}
-  
-  var findChildren = function(tr) {
-    var depth = tr.dataset.depth;
-    var elements = [...document.querySelectorAll('#boq tr')].filter(function(element) {
-      return element.dataset.depth <= depth;
-    });
-    var next = nextUntil(tr, elements);
-    return next;
-  };
-  
-  var nextUntil = function(elem, elements, filter) {
-    var siblings = [];
-    elem = elem.nextElementSibling;
-    while (elem) {
-      if (elements.includes(elem)) break;
-      if (filter && !elem.matches(filter)) {
-        elem = elem.nextElementSibling;
-        continue;
-      }
-      siblings.push(elem);
-      elem = elem.nextElementSibling;
-    }
-    return siblings;
-  };
+  })
+}
 
-  function removeAllChildren(element) {
-    while (element.firstChild) {
-        element.removeChild(element.firstChild);
+var findChildren = function (tr) {
+  var depth = tr.dataset.depth;
+  var elements = [...document.querySelectorAll('#boq tr')].filter(function (element) {
+    return element.dataset.depth <= depth;
+  });
+  var next = nextUntil(tr, elements);
+  return next;
+};
+
+var nextUntil = function (elem, elements, filter) {
+  var siblings = [];
+  elem = elem.nextElementSibling;
+  while (elem) {
+    if (elements.includes(elem)) break;
+    if (filter && !elem.matches(filter)) {
+      elem = elem.nextElementSibling;
+      continue;
     }
+    siblings.push(elem);
+    elem = elem.nextElementSibling;
   }
+  return siblings;
+};
+
+function removeAllChildren(element) {
+  while (element.firstChild) {
+    element.removeChild(element.firstChild);
+  }
+}
 
